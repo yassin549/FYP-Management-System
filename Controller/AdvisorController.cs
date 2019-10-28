a55a63ca9d47ec5cfd75dfa27fe9a7ab012805e7
yassin549 <khoualdiyassin26@gmail.com>
2019-10-28 15:30:18 +0500

    Enhanced UI with better layout and improved navigation

---
+++ Controller/AdvisorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Controller
+{
+	internal static class AdvisorController
+	{
+		public static int addAdvisor(SqlConnection con, string salary, int designation)
+		{
+			int id = PersonController.getLastPersonId();
+			SqlCommand AdvisorCmd = new SqlCommand("INSERT INTO Advisor (Id,Designation,Salary)" +
+														" VALUES (@Id, @Designation, @Salary)", con);
+			AdvisorCmd.Parameters.AddWithValue("@Id", id);
+			AdvisorCmd.Parameters.AddWithValue("@Salary", salary);
+			AdvisorCmd.Parameters.AddWithValue("@Designation", designation);
+			AdvisorCmd.ExecuteNonQuery();
+			return id;
+		}
+
+		public static bool inAdvisorTable(string id)
+		{
+			if (DbController.getUserIdFromColumn("Advisor", "Id", id) != null)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
---
+++ Controller/DbController.cs
@@ -0,0 +1,61 @@
+﻿using CRUD_Operations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+using static System.Collections.Specialized.BitVector32;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace WindowsFormsApp1.Controller
+{
+	internal static class DbController
+	{
+		public static string getFromTable(string columns, string table, string query)
+		{
+			var connection = Configuration.getInstance().getConnection();
+			SqlCommand sqlCommand = new SqlCommand("Select " + columns + " from " + table + " " + query, connection);
+
+			var x = sqlCommand.ExecuteScalar();
+			sqlCommand.ExecuteNonQuery();
+			return x == null ? null : x.ToString();
+		}
+
+		public static void QueryExecuter(string query)
+		{
+			var connection = Configuration.getInstance().getConnection();
+
+			SqlCommand sqlCommand = new SqlCommand(query, connection);
+			sqlCommand.ExecuteNonQuery();
+
+		}
+
+
+		public static string getUserIdFromColumn(string table, string column, string value)
+		{
+			var con = Configuration.getInstance().getConnection();
+			SqlCommand sqlCommand = new SqlCommand("select id from "+ table +" where " + column + " = @column", con);
+			sqlCommand.Parameters.AddWithValue("@column", value);
+			var x = sqlCommand.ExecuteScalar();
+			sqlCommand.ExecuteNonQuery();
+			return x == null ? null : x.ToString();
+
+		}
+
+		public static int getLastId(string table)
+		{
+			var con = Configuration.getInstance().getConnection();
+			SqlCommand cmd = new SqlCommand("SELECT TOP 1 Id FROM "+table+" ORDER BY Id DESC", con);
+			int lastId;
+
+			var result = cmd.ExecuteScalar();
+			lastId = result == null ? 1 : (int)result;
+
+			return lastId;
+		}
+	}
+}
