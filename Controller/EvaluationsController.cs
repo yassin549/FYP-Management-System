1c2f266dd6ed7baf1bd952a8b7c2bddd969653d8
yassin549 <khoualdiyassin26@gmail.com>
2019-10-30 13:14:12 +0500

    Added search and filter functionality for students and projects

---
+++ Controller/EvaluationsController.cs
@@ -0,0 +1,25 @@
+﻿using CRUD_Operations;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+
+namespace WindowsFormsApp1.Controller
+{
+	internal static class EvaluationsController
+	{
+		public static string getEvaluationMetric(string name, string column)
+		{
+			var con = Configuration.getInstance().getConnection();
+			SqlCommand Command = new SqlCommand("select " + column + " from Evaluation where Name = @name", con);
+			Command.Parameters.AddWithValue("@name", name);
+			var y = Command.ExecuteScalar();
+			Command.ExecuteNonQuery();
+			return y == null ? null : y.ToString();	
+		}
+	}
+}
---
+++ Controller/PersonController.cs
@@ -0,0 +1,35 @@
+﻿using CRUD_Operations;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Controller
+{
+	internal static class PersonController
+	{
+		public static int getLastPersonId()
+		{
+			var con = Configuration.getInstance().getConnection();
+			SqlCommand cmd = new SqlCommand("SELECT TOP 1 Id FROM Person ORDER BY Id DESC", con); 
+			int lastId;
+
+			var result = cmd.ExecuteScalar();
+			lastId = result == null ? 1 : (int)result;
+
+			return lastId;
+		}
+
+		public static string getCredentials(string credential, string value)
+		{
+			var con = Configuration.getInstance().getConnection();
+			SqlCommand sqlCommand = new SqlCommand("select " + credential + " from Person where id = @column", con);
+			sqlCommand.Parameters.AddWithValue("@column", value);
+			var x = sqlCommand.ExecuteScalar();
+			sqlCommand.ExecuteNonQuery();
+			return x == null ? null : x.ToString();
+		}
+	}
+}
