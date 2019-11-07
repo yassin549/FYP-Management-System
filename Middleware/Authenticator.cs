060a623eb743c32a90b0d8db0395391bd08f664f
yassin549 <khoualdiyassin26@gmail.com>
2019-11-07 13:42:22 +0500

    Implemented middleware for Authentication and Validation

---
+++ Middleware/Authenticator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Controller;
+
+namespace WindowsFormsApp1.Middleware
+{
+	internal static class Authenticator
+	{
+		public static bool isStudent(string id)
+		{
+			return StudentController.inStudentTable(id);
+		}
+
+		public static bool isAdvisor(string id)
+		{
+			return AdvisorController.inAdvisorTable(id);
+		}
+	}
+}
---
+++ Middleware/Validations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.Middleware
+{
+	internal static class Validations
+	{
+		public static bool EndsWith<T>(this T text, string suffix)
+		{
+			if (text == null || suffix == null)
+			{
+				return false;
+			}
+
+			return text.ToString().ToLower().EndsWith(suffix.ToLower());
+		}
+
+		public static int validIntFromCB(ComboBox comboBox)
+		{
+			if (comboBox.Text == String.Empty)
+			{
+				return -1;
+			}
+			return int.Parse(comboBox.Text);
+		}
+	}
+}
